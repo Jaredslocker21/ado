049bf5b2dce733f8208446899f7a1d69e09bc39f
Jaredslocker21 <jaredslocker21@gmail.com>
2023-01-12 11:29:38 +0000

    fuck

--- checkout/views.py
+++ checkout/views.py
@@ -3,7 +3,6 @@ from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
-
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -12,21 +11,20 @@ from bag.contexts import bag_contents
 import stripe
 import json
 
-
 @require_POST
 def cache_checkout_data(request):
     try:
-        pid = request.POST.get('client_secret').split('_secret')[0],
-        stripe.api_key = settings.STRIPE_SECRET_KEY,
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dump(request.session.get('bag', {})),
+            'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
-            'user_name': request.user
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry your payment cannot be accepted \
-            at this time. Please try again later.')
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
