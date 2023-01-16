7d3d2af12697954fbf0b0436b7489a529c42aa9f
Jaredslocker21 <jaredslocker21@gmail.com>
2023-01-16 21:41:41 +0000

    secret key removed

--- ado/settings.py
+++ ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i205n&lomfu(gbvot9m)7il&ih20fj!6y_492u5rl+x5%se*7x'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ado.herokuapp.com', 'localhost']
 
