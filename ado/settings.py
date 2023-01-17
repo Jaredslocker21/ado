fda8f5c891fd0ffe42eb9d9953749bd64bcb1567
Jaredslocker21 <jaredslocker21@gmail.com>
2023-01-17 00:11:28 +0000

    deployment cloud storage

--- ado/settings.py
+++ ado/settings.py
@@ -177,23 +177,22 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 if 'USE_AWS' in os.environ:
-    # bucket config
-    AWS_STORAGE_BUCKET_NAME = 'walkthrough.ado'
-    AWS_S3__REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_EKY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'walkthroughado'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazon.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # static and media file storage
-    STATICFILE_STORAGE = 'custom_storages.StaticStorage'
-    STATIC_FILES_LOCATION = 'static'
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIA_FILES_LOCATION = 'media'
+    MEDIAFILES_LOCATION = 'media'
 
-    # Override static and media URLS in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_FILES_LOCATION}/'
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
