61f34d2108fe3cc9bf2aed0bd7c8640c3174b9fb
Jaredslocker21 <jaredslocker21@gmail.com>
2023-01-19 09:05:52 +0000

    did it

--- ado/settings.py
+++ ado/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ado.herokuapp.com', 'localhost']
 
