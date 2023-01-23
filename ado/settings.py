32732afaa3800ad72729f7b6ec0a8faf57c1d601
Jaredslocker21 <jaredslocker21@gmail.com>
2023-01-23 13:27:45 +0000

    Revert " yea yeah"
    This reverts commit ac765f5ad02b3ccd6b8ba0ce67fb83d874ad3f4d.

--- ado/settings.py
+++ ado/settings.py
@@ -201,7 +201,7 @@ if 'USE_AWS' in os.environ:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-fields
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
